8cccc511099b27feec9d9194a7c7a67046589884
Rahelaraya <rahelaraya26@gmail.com>
2025-03-14 10:27:03 +0100

    Add project files.

---
+++ FredagKodtest/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace FredagKodtest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
