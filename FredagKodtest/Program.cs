102a034365fe9649624bd16796251d3a7b0eec62
Rahelaraya <rahelaraya26@gmail.com>
2025-03-14 10:59:22 +0100

    try

--- FredagKodtest/Program.cs
+++ FredagKodtest/Program.cs
@@ -1,10 +1,46 @@
-﻿namespace FredagKodtest
+﻿using System.Text.RegularExpressions;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace FredagKodtest
 {
-    internal class Program
+    class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            var input = new List<string> { "listen", "silent", "enlist", "rat", "tar", "art", "evil", "vile", "live" };
+            var resultat = GrupperaAnagram(input);
+
+            // Skriv ut resultatet
+            foreach (var grupp in resultat)
+            {
+                Console.WriteLine($"[{string.Join(", ", grupp)}]");
+            }
         }
+        public static List<List<string>> GrupperaAnagram(List<string> ordLista)
+            {
+                Dictionary<string, List<string>> anagramGrupper = new Dictionary<string, List<string>>();
+
+                foreach (string ord in ordLista)
+                {
+                    // Sortera bokstäverna i ordet för att få en unik nyckel
+                    string sorteratOrd = new string(ord.OrderBy(c => c).ToArray());
+
+                    // Använd TryGetValue för effektiv dictionary-uppslagning
+                    if (!anagramGrupper.TryGetValue(sorteratOrd, out List<string> grupp))
+                    {
+                        grupp = new List<string>();
+                        anagramGrupper[sorteratOrd] = grupp;
+                    }
+                    grupp.Add(ord);
+                }
+
+                return anagramGrupper.Values.ToList();
+            }
+
+            
+        
+
     }
 }
+
+
